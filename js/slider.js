94402da36e244929b15ee471728b8bfa36398b46
SergiuszStyczen <sergiusz.styczen@gmail.com>
2017-04-05 16:56:19 +0200

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,18 @@
+var imgCount = 1;
+var total = 3;
+
+function slide(x){
+    var currentImage = document.getElementById('img');
+    imgCount = imgCount + x;
+    if(imgCount > total){imgCount = 1}
+    if(imgCount < 1){imgCount = total}
+    currentImage.src = "img/img" + imgCount + ".jpg";
+}
+
+window.setInterval(function loadSlide(){
+    var currentImage = document.getElementById('img');
+    imgCount = imgCount + 1;
+    if(imgCount > total){imgCount = 1}
+    if(imgCount < 1){imgCount = total}
+    currentImage.src = "img/img" + imgCount + ".jpg";
+}, 3000);
